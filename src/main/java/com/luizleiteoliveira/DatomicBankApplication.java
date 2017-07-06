dab438c0429eeaee868d6e60af0e8fd46a8c943f
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-07-06 06:28:33 -0300

    init from project from datomic bank

---
+++ src/main/java/com/luizleiteoliveira/DatomicBankApplication.java
@@ -0,0 +1,12 @@
+package com.luizleiteoliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatomicBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatomicBankApplication.class, args);
+	}
+}
