97a116dac84954f6691b02ceaca4b0e52bbb32d0
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-07-06 10:40:30 -0300

    begin inserts

--- src/main/java/DAO/Move.java
+++ src/main/java/DAO/Move.java
@@ -1,9 +1,13 @@
 package DAO;
 
 import datomic.Connection;
+import datomic.Database;
+
 import java.io.IOException;
 
 import static DAO.Fns.scratchConnection;
+import static DAO.IO.transactAllFromResource;
+import static datomic.Util.list;
 
 /**
  * Created by luizleite on 06/07/17.
@@ -12,5 +16,11 @@ public class Move {
 
     public static void main(String[] args) throws IOException {
         Connection conn = scratchConnection();
+        transactAllFromResource(conn, "datomic_bank/move-schema.edn");
+        Database db = conn.db();
+    }
+
+    public void pullMove(String userId, Long value, String transaction) {
+        Object move = list("")
     }
 }
